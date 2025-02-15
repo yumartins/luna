24760e06819e76865a1eae986dcda2463aa6a9d5
Yuri Martins <develop@yumartins.com.br>
2025-02-15 09:12:12 -0300

    feat(api): add better-auth lib and setup docker compose file

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,13 @@
 import { Elysia } from "elysia";
+import { swagger } from '@elysiajs/swagger'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(swagger())
+  .get("/", () => "Hello Elysia")
+  .listen(3333);
+
+if (!app.server) throw new Error("Server not initialized");
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+	`🦊 Elysia is running at ${app.server.hostname}:${app.server.port}`,
 );
